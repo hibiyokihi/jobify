dc93aad2cd1acdd9ac053ce7b6189ad6b6b7a1b5
Yusuke Mizumoto <y.mizumoto9@gmail.com>
2024-02-19 22:43:26 +0700

    completed lesson 88

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,6 +1,8 @@
-import { body, validationResult } from 'express-validator';
+import { body, param, validationResult } from 'express-validator';
 // joiと同じく、validationを行うパッケージ。request.bodyを見に行くからbodyを取り出してる。
 import { BadRequestError } from '../errors/customErrors.js';
+import { JOB_STATUS, JOB_TYPE } from '../utils/constants.js';
+import mongoose from 'mongoose';
 
 const withValidationErrors = (validateValues) => {
   // express-validatorミドルウェアでvalidationする際、このFnを使い回すことができる。引数のvalidationだけ変えればOK。
@@ -23,16 +25,25 @@ const withValidationErrors = (validateValues) => {
 };
 
 export const validateJobInput = withValidationErrors([
-  
-])
+  body('company').notEmpty().withMessage('company is required'),
+  // req.bodyから'company'を探して、notEmptyがfalseなら(emptyであるなら)、errors.arrayにエラー情報を入れる。
+  // validationは必要な数だけチェーンできる。
+  body('position').notEmpty().withMessage('position is required'),
+  body('jobLocation').notEmpty().withMessage('job location is required'),
+  body('jobStatus')
+    .isIn(Object.values(JOB_STATUS))
+    .withMessage('invalid status value'),
+  // enum(選択肢から選ぶ)をvalidateする時はisInを使う。JOB_STATUSに規定した選択肢から選ぶのだからisInのはず。
+  body('jobType')
+    .isIn(Object.values(JOB_TYPE))
+    .withMessage('invalid type value'),
+]);
 
-// export const validateTest = withValidationErrors([
-//   body('name')
-//     .notEmpty()
-//     .withMessage('name is required')
-//     .isLength({ min: 3, max: 100 })
-//     .withMessage('name must be between 3 and 100 characters')
-//     .trim(),
-//   // req.bodyから'name'を探して、notEmptyがfalseなら(emptyであるなら)、エラーメッセージを流す。
-//   // validationは必要な数だけチェーンできる。
-// ]);
+export const validateIdParam = withValidationErrors([
+  param('id')
+    .custom((value) => mongoose.Types.ObjectId.isValid(value))
+    // 'id'はrouterで設定した/:id。customの引数名は任意で、param上のidの値が渡される。
+    // valueが、MongoDBが作成するObjectIdの型と一致するかをvalidateする。idの文字数を増減させるとこのエラーが発生。
+    // isValidがfalseの場合、エラー情報がerrors.arrayに渡されて、次のミドルにはnextされず、処理が終了する。
+    .withMessage('invalid MongoDB id'),
+]);
--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -8,10 +8,21 @@ import {
   updateJob,
   deleteJob,
 } from '../controllers/jobController.js';
+import {
+  validateIdParam,
+  // paramとして渡した':id'が、MongoDBが作成したIDとして正しい形式かvalidateするミドル。
+  validateJobInput,
+  // post, patchリクエスト時の入力内容をvalidateするミドル
+} from '../middleware/validationMiddleware.js';
 
-router.route('/').get(getAllJobs).post(createJob)
+router.route('/').get(getAllJobs).post(validateJobInput, createJob);
 // /api/v1/jobs/ に対してget, postがあったらそれぞれの関数が対応する。
+// ミドルウェアがある場合は、処理関数の前に書く。ここではpostデータをvalidationするミドルウェアが入る。
 // router.get('/', getAllJobs)の方法で一つずつ書いても良いか、この方が書く量が少なくて済む。
-router.route('/:id').get(getJob).patch(updateJob).delete(deleteJob)
+router
+  .route('/:id')
+  .get(validateIdParam, getJob)
+  .patch(validateJobInput, validateIdParam, updateJob)
+  .delete(validateIdParam, deleteJob);
 
-export default router
+export default router;
